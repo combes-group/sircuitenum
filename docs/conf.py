5ad631f2f02d0994e3badbf14c06d419a7527224
Eli Weissler <eliweissler@gmail.com>
2023-06-13 18:58:41 -0600

    updated build

--- docs/conf.py
+++ docs/conf.py
@@ -14,16 +14,9 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser",
-              'sphinx.ext.autodoc',
-'sphinx.ext.doctest',
-'sphinx.ext.intersphinx',
-'sphinx.ext.todo',
-'sphinx.ext.coverage',
-'sphinx.ext.mathjax',
-'sphinx.ext.ifconfig',
-'sphinx.ext.viewcode',
-'sphinx.ext.githubpages']
+extensions = [
+    "myst_parser",
+    'sphinx.ext.mathjax']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
